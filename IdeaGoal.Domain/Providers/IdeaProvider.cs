9b6f0809916205c4873bb7f9a7f186acee3d2ace
Grigor <grigdevelop@gmail.com>
2018-11-07 17:57:17 +0100

    temp changes

---
+++ IdeaGoal.Domain/Providers/IdeaProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdeaGoal.Domain.Providers
+{
+    public class IdeaProvider
+    {
+        public IdeaProvider()
+        {
+
+        }
+    }
+}
---
+++ IdeaGoal.Domain/Services/Ideas/Dto/IdeaDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdeaGoal.Domain.Services.Ideas.Dto
+{
+    public class IdeaDto
+    {
+    }
+
+    public class CreateIdeaDto
+    {
+        public int UserId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class UpdateIdeaDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Id { get; set; }
+    }
+}
